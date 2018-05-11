abcf115c1cc5d5972d2c4f2105d3d560576233e4
Thomas Roch <thomas.c.roch@gmail.com>
2018-05-11 16:31:58 +0100

    feat: add mapSetStateToProps argument

--- modules/state.js
+++ modules/state.js
@@ -1,20 +1,22 @@
 import { createElement, Component } from 'react';
 
-function state(initialState = {}) {
+function state(initialState = {}, mapSetStateToProps = () => () => ({})) {
     return function (StatelessComponent) {
         class StateHoc extends Component {
             constructor(props) {
                 super(props);
                 this.state = typeof initialState === 'function' ? initialState(props) : initialState;
                 this.setState = this.setState.bind(this);
+                this.mappedProps = mapSetStateToProps(this.props)(this.setState);
             }
 
             render() {
-                const { state, props, setState } = this;
+                const { state, props, setState, mappedProps } = this;
 
                 return createElement(StatelessComponent, {
                     ...props,
                     ...state,
+                    ...mappedProps,
                     setState
                 });
             }
