4be3fea2d4804fd9e37f5712e4f35324139cf62e
Joe McGrath <joe.mcgrath@fanduel.com>
2018-06-13 23:48:12 +0100

    test: add more tests for withState

--- modules/__tests__/withState.tsx
+++ modules/__tests__/withState.tsx
@@ -1,30 +1,95 @@
 import * as React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import withState from '../withState'
 
 describe('withState', () => {
     it('should handle state as a HoC', () => {
-        interface State {
-            visible: boolean
-        }
-
-        interface ExtraProps {
-            setVisibility: (visible: boolean) => { visible: boolean }
-        }
-
-        const Component = ({ visible, setVisibility }) => (
-            <button onClick={() => setVisibility(!visible)}>Toggle</button>
+        const Component: any = ({ visible, setState }) => (
+            <button onClick={() => setState({ visible: !visible })}>
+                Toggle
+            </button>
         )
 
-        const FinalComponent = withState<{}, State, ExtraProps>(
-            { visible: false },
-            { setVisibility: visible => ({ visible }) }
-        )(Component)
+        const ComponentWithState = withState({ visible: false })(Component)
 
-        const component = shallow(<FinalComponent />)
+        const wrapper = mount(<ComponentWithState />)
+        const component = wrapper.find(Component)
 
         expect(component.prop('setState')).toBeDefined()
         expect(component.prop('visible')).toBe(false)
+        expect(wrapper.instance().state).toEqual({ visible: false })
+
+        component.find('button').prop('onClick')({} as any)
+
+        expect(wrapper.instance().state).toEqual({ visible: true })
+    })
+
+    it('should accept an object of state creators', () => {
+        const Component: any = ({ visible, setVisibility }) => (
+            <button onClick={() => setVisibility(!visible)}>Toggle</button>
+        )
+
+        const ComponentWithState = withState(
+            { visible: false },
+            {
+                setVisibility: visible => ({ visible })
+            }
+        )(Component)
+
+        const wrapper = mount(<ComponentWithState />)
+        const component = wrapper.find(Component)
+
         expect(component.prop('setVisibility')).toBeDefined()
+        expect(wrapper.instance().state).toEqual({ visible: false })
+
+        component.find('button').prop('onClick')({} as any)
+
+        expect(wrapper.instance().state).toEqual({ visible: true })
+    })
+
+    it('should accept a mapSetStateToProps function', () => {
+        const Component: any = ({ visible, setVisibility }) => (
+            <button onClick={() => setVisibility(!visible)}>Toggle</button>
+        )
+
+        const ComponentWithState = withState(
+            { visible: false },
+            () => setState => ({
+                setVisibility: visible => setState({ visible })
+            })
+        )(Component)
+
+        const wrapper = mount(<ComponentWithState />)
+        const component = wrapper.find(Component)
+
+        expect(component.prop('setVisibility')).toBeDefined()
+        expect(wrapper.instance().state).toEqual({ visible: false })
+
+        component.find('button').prop('onClick')({} as any)
+
+        expect(wrapper.instance().state).toEqual({ visible: true })
+    })
+
+    it('should accept a custom mergeProps function', () => {
+        const Component: any = ({ visible, setVisibility }) => (
+            <button onClick={() => setVisibility(!visible)}>Toggle</button>
+        )
+
+        const ComponentWithState = withState(
+            { visible: false },
+            null,
+            (state, setStateProps, initialProps) => ({
+                state,
+                initialProps
+            })
+        )(Component)
+
+        const wrapper = mount(<ComponentWithState />)
+        const component = wrapper.find(Component)
+
+        expect(component.props()).toEqual({
+            state: { visible: false },
+            initialProps: {}
+        })
     })
 })
